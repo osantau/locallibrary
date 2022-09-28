6970c8eef4e62d3c79edfbfe93858f36f0ad8e07
octsantau <octavian.santau@plastor.ro>
2022-09-28 10:22:13 +0300

    Define urls for catalog. Add static to urlpatterns.

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # My Apps
-    'catalog',
+    # Add our new application
+    'catalog.apps.CatalogConfig',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
