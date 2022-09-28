0495c635dc38207e461b7a18d4963dc0a3a5fef2
octsantau <octavian.santau@plastor.ro>
2022-09-28 14:34:19 +0300

    Add models to admin.

--- catalog/models.py
+++ catalog/models.py
@@ -33,7 +33,7 @@ class Book(models.Model):
     isbn = models.CharField('ISBN', max_length=13, unique=True,
                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(
-        Genre, help_text='Select a genre for this book')
+        'Genre', help_text='Select a genre for this book')
     language = models.ForeignKey(
         'Language', on_delete=models.SET_NULL, null=True)
 
