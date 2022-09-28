f98e09f7a37785186a834200f62db4f93fb835ce
octsantau <octavian.santau@plastor.ro>
2022-09-28 14:54:36 +0300

    Add initial styles.

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Author, Book, BookInstance, Genre
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Catalog Welcome')
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
